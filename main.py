00291a28a93a09a625de97257055d5f5164c72e7
Elior Basli <45131527+Eliorbasli@users.noreply.github.com>
2022-11-02 13:20:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+
+def get_exchange(symbol = "EUR", number = 10 , headers={}):
+  symbol = "EUR"
+  url = "https://api.apilayer.com/exchangerates_data/latest?symbols=&base${symbol}"
+  payload = {}
+
+  response = requests.request("GET", url, headers=headers, data = payload)
+  status_code = response.status_code
+  result = json.loads(response.text)
+  x = result["rates"]
+
+  under_number_set = {""}
+  for keys , values  in x.items():
+    if(values < number ):
+      under_number_set.add(keys)
+
+  return(under_number_set)
+
+def main():
+  headers= {
+    "apikey": "YOUR_API"
+  }
+
+  print("-----check1------")
+  print(get_exchange(number = 10, symbol ="GBP" , headers = headers))
+  print("-----check2------")
+  print(get_exchange(headers = headers))
+  print("-----check3------")
+  print(get_exchange(symbol ="EUR",number=20 , headers = headers))
+
+
+main()
